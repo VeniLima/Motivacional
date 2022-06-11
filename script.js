cdd15e5dba4727500faaf2f062d1c780d8237dae
Vinicius Lima <vinicius.lima.brasil1@gmail.com>
2022-06-11 13:40:43 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const phrases = [
+  "Dias ruins são necessários para termos dias piores.",
+  "Lute como nunca, perca como sempre.",
+  "Nenhum obstáculo é grande para quem desiste.",
+  "O caminho é longo, mas a derrota é certa.",
+  "É só uma fase ruim, logo vai piorar.",
+  "Seja o protagonista do seu fracasso.",
+  "Você é mais fraco do que pensa.",
+  "Nunca foi azar, sempre foi incompetência.",
+  "Não pare, faça até dar errado.",
+  "Acredite. Você nasceu para conquistar grander fracassos.",
+  "Você NÃO é incrível.",
+  "Coisas ruins se vão para que piores possam vir.",
+  "Seu maior problema é você.",
+  "Quando sua mente pensar em desistir, desista",
+  "Só dará errado se você tentar.",
+  "Para ficar ruim, você terá que melhorar muito.",
+  "Acreditar que você pode já é meio caminho errado.",
+  "Nunca é tarde para desistir.",
+  "Você não pode mudar o seu passado. Mas pode estragar o seu futuro.",
+  "O recomeço é uma oportunidade de um novo fracasso",
+  "Durmam 12 horas por dia, assim as chances de fazer merda caem pela metade",
+];
+
+function sendMessage() {
+  const totalPhrases = phrases.length;
+  const random = Math.floor(Math.random() * totalPhrases);
+  document.querySelector("#message").innerHTML = phrases[random];
+}
